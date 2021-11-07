8b893a183f5af78f51a0d5f6524bd486a2d22a5e
Veerwhitehat <92624033+Veerwhitehat@users.noreply.github.com>
2021-11-07 11:18:43 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+const { fabric } = require("./fabric");
+
+var canvas= new fabric.Canvas("myCanvas");
+
+var block_image_width= 30;
+var block_image_height= 30;
+
+player_x=10;
+player_y=10;
+
+var player_object="";
+var block_object="";
+
+function playerupload(){
+    fabric.Image.fromURL("player.png",function(Img){
+      player_object=Img;
+      player_object.scaleToWidth(150); 
+      player_object.scaleToHeight(140);
+      
+      player_object.set({
+          top:player_y,
+          left:player_x,
+
+      });
+      canvas.add(player_object);
+    });
+}
+function objectupload(block_image){
+    fabric.Image.fromURL(block_image,function(Img){
+      block_object=Img;
+      block_object.scaleToWidth(block_image_width); 
+      block_object.scaleToHeight(block_image_height);
+      
+      block_object.set({
+          top:player_y,
+          left:player_x,
+
+      });
+      canvas.add(block_object);
+    });
+}
